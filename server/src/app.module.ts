615fde2b80f6ce5a01a290ea25869f3c37e909a3
Netanel <natanelmich103@gmail.com>
2022-10-08 20:35:02 +0300

    Minor: mailer-module will now be used only inside notification-module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common'
 // configs
-import { ConfigsModuleOptions, MailerModuleAsyncOptions } from './configs/module-options'
+import { ConfigsModuleOptions } from './configs/module-options'
 // modules
 import { ConfigModule } from '@nestjs/config'
-import { MailerModule } from '@nestjs-modules/mailer'
 import {
   ArticleModule,
   AuthModule,
@@ -19,7 +18,6 @@ import {
   imports: [
     // global modules:
     ConfigModule.forRoot(ConfigsModuleOptions),           // -- config module - access environment variables globally.
-    MailerModule.forRootAsync(MailerModuleAsyncOptions),  // -- mailer module - allows sending mails globally.
     PrismaModule,                                         // -- prisma module - access to main database globally.
     JwtModule,                                            // -- jwt module - access normal & custom jwt-methods globally.
     NotificationModule,                                   // -- notification module - push notifications.
--- server/src/modules/notification/notification.module.ts
+++ server/src/modules/notification/notification.module.ts
@@ -1,8 +1,14 @@
 import { Module, Global } from '@nestjs/common'
+// configs
+import { MailerModuleAsyncOptions } from '../../configs/module-options'
+// mailer-module (will be used inside notification-module)
+import { MailerModule } from '@nestjs-modules/mailer'
+// services
 import { NotificationService } from './notification.service'
 
 @Global()
 @Module({
+    imports: [MailerModule.forRootAsync(MailerModuleAsyncOptions)],
     providers: [NotificationService],
     exports: [NotificationService]
 })
